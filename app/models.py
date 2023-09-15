5daf0aab0f466951a3377d1fc01a3a8d25a77af3
thiagooruby <trs3@aluno.ifal.edu.br>
2023-09-15 19:04:09 -0300

    add followers table to db and posts in the website

--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ from flask_login import UserMixin
 from app import login
 from hashlib import md5
 
+# useri (seguidor) segue userj (seguido)
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')),)
+
 # UserMixin inclui implementações genéricas que são apropriadas para a maioria das classes de modelo de usuário.
 class User(UserMixin, db.Model):
 
@@ -16,7 +21,18 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy = 'dynamic'
+    )
+
+    # followed = quem o usuario segue
+    # followers = quem segue o usuario
 
     def __repr__(self):
         return f'<User {{{self.username}}}>'
@@ -31,7 +47,28 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
 
         return f'https://www.gravatar.com/avatar/{digest}?d=monsterid&s={size}'
+    
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    # Pega os posts dos seguidos
+    def followed_posts(self):
+        '''
+        Faz um join de Posts e followers pelo indice do usuario seguidoe e filtra para a condição onde o seguidor é o usuario atual, ordenando com base no tempo
+        '''
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id = self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 class Post(db.Model):
 
@@ -42,7 +79,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {{{self.body}}}>'
-    
+
 
 # Carrega um usuario dado id para o flask-login identificar o usuario logado
 @login.user_loader
